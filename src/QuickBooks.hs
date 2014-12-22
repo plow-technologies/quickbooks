29d0aa80ecae01990ef956e9ef36d980a5b68988
externalreality <ejones@stackbuilders.com>
2014-12-22 18:18:13 -0500

    [83349058] Create instances and additional types.

--- src/QuickBooks.hs
+++ src/QuickBooks.hs
@@ -1,29 +1,43 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs          #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE DeriveGeneric  #-}
 
 module QuickBooks where
 
 import System.Environment (getEnv)
+import Network.Wreq
+import Data.Monoid
+import Data.Aeson
+import GHC.Generics
 
 data SalesItemLineDetail = SalesItemLineDetail
+  deriving (Generic)
 
 data DetailType = DetailType
+  deriving (Generic)
 
 data Line = Line
   { lineId                  :: !(Maybe String)
   , lineAmount              :: !Double
   , lineDetailType          :: !DetailType
   , lineSalesItemLineDetail :: !SalesItemLineDetail
-  }
+  } deriving (Generic)
 
 data CustomerRef = CustomerRef
   { customerRefValue :: !String
-  }
+  } deriving (Generic)
 
 data Invoice = Invoice
   { invoiceId          :: !(Maybe String)
   , invoiceLines       :: ![Line]
   , invoiceCustomerRef :: !CustomerRef
-  }
+  } deriving (Generic)
+
+instance ToJSON SalesItemLineDetail
+instance ToJSON DetailType
+instance ToJSON Line
+instance ToJSON CustomerRef
+instance ToJSON Invoice
 
 data APIKey = APIKey
 
@@ -38,6 +52,12 @@ data APIConfig = APIConfig
   , apiKey    :: String
   }
 
-runQuickBooksRequest :: QuickBooksRequest -> IO ()
-runQuickBooksRequest (CreateInvoice invoice) = undefined
--- runQuickBooksRequest _ = undefined
+runQuickBooksRequest :: (?apiConfig :: APIConfig) => QuickBooksRequest -> IO ()
+runQuickBooksRequest (CreateInvoice invoice) = do 
+  post createInvoiceURL (toJSON invoice)
+  return ()
+
+createInvoiceURL :: (?apiConfig :: APIConfig) => String
+createInvoiceURL = "/v3/company/" <> companyId ?apiConfig <> "/invoice"
+
+
