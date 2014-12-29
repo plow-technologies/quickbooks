648c829e52f3f586950c7851e79f94b18361016f
externalreality <ejones@stackbuilders.com>
2014-12-29 12:44:59 -0500

    [83349058] Initial test.

--- test/QuickBooksSpec.hs
+++ test/QuickBooksSpec.hs
@@ -4,4 +4,11 @@ import QuickBooks
 import Test.Hspec
 
 spec :: Spec
-spec = undefined
+spec = quickBooksAPISpec
+
+quickBooksAPISpec :: Spec
+quickBooksAPISpec = describe "createInvoice" $ do
+  it "requests the creation of an invoice given and invoice and authorization credentials." createInvoiceTest
+
+createInvoiceTest :: Bool
+createInvoiceTest = undefined
