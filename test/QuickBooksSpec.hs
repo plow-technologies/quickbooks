551784a1b03a3a0510b5582cf74f2c14c16a44ab
externalreality <ejones@stackbuilders.com>
2015-01-01 00:59:36 -0500

    [83349058] Remove test environment secrets from source code.

--- test/QuickBooksSpec.hs
+++ test/QuickBooksSpec.hs
@@ -6,7 +6,6 @@ import Control.Monad (void)
 import QuickBooks
 import Test.Hspec
 import QuickBooks.Types
-import System.Environment
 import Data
 import Data.Maybe
 
@@ -15,25 +14,16 @@ spec = quickBooksAPISpec
 
 quickBooksAPISpec :: Spec
 quickBooksAPISpec = do
-  describe "QuickBooks API" $ do
+  describe "QuickBooks API Binding" $ do
     it "queries quickbooks to create a new invoice."    createInvoiceTest
     it "queries an invoice given an invoice Identifier" readInvoiceTest
     it "queries quickbooks to update and invoice."      updateInvoiceTest
     it "queries quickbooks to delete and invoice."      deleteInvoiceTest
     it "gets temporary tokens."                         getTempTokensTest
 
-setTestEnv :: IO ()
-setTestEnv = do
- setEnv "INTUIT_COMPANY_ID"      "1315190090"
- setEnv "INTUIT_CONSUMER_KEY"    "qyprdJsVJQN726R7DY4QW7S9V5YUze"
- setEnv "INTUIT_CONSUMER_SECRET" "c1B6XtDtn8kFjrqRBhHovu7IlpX59sHCPqJFXvMi"
- setEnv "INTUIT_TOKEN"           "lvprdeMmQMNWSSPzSkF65TGhC3C6NYSGiiDFuNB3q4Szs6TC"
- setEnv "INTUIT_SECRET"          "QOLKz5xg0wjGsdiqUbRE7wLZgrgmcs2X7Zy9JFpl"
- setEnv "INTUIT_HOSTNAME"        "sandbox-quickbooks.api.intuit.com"
 
 getTempTokensTest :: Expectation
 getTempTokensTest = do
-  setTestEnv
   temporaryTokens <- getTempTokens "localhost"
   case temporaryTokens of
     Left err -> print err
@@ -41,7 +31,6 @@ getTempTokensTest = do
 
 createInvoiceTest :: Expectation
 createInvoiceTest = do
-  setTestEnv
   quickBooksInvoiceResponse <- createInvoice testInvoice
   case quickBooksInvoiceResponse of
     Left err -> print err
@@ -51,7 +40,6 @@ createInvoiceTest = do
           
 readInvoiceTest :: Expectation
 readInvoiceTest = do
-  setTestEnv
   quickBooksInvoiceResponse <- createInvoice testInvoice
   case quickBooksInvoiceResponse of
     Left err -> print err
@@ -64,7 +52,6 @@ readInvoiceTest = do
 
 updateInvoiceTest :: Expectation
 updateInvoiceTest = do 
-  setTestEnv
   quickBooksInvoiceResponse <- createInvoice testInvoice
   case quickBooksInvoiceResponse of
     Left err -> print err
@@ -77,7 +64,6 @@ updateInvoiceTest = do
 
 deleteInvoiceTest :: Expectation
 deleteInvoiceTest = do
-  setTestEnv
   quickBooksInvoiceResponse <- createInvoice testInvoice
   case quickBooksInvoiceResponse of
     Left err -> print err
