5dbacd9a6d5b1557c8ab016cc0009d685bf3d941
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-01-05 12:59:03 -0500

    [83349058] Fix bug in token exchange function

--- src/QuickBooks/Authentication.hs
+++ src/QuickBooks/Authentication.hs
@@ -10,11 +10,12 @@ module QuickBooks.Authentication
 
 import Control.Applicative
 import qualified Data.ByteString.Lazy as BSL
-import Data.Text (unpack)
+import Data.ByteString.UTF8 (toString)
+import Data.Text.Encoding (encodeUtf8)
 import QuickBooks.Types
 import Network.HTTP.Client (Manager, Request(..), RequestBody(RequestBodyLBS), responseBody, parseUrl, httpLbs)
 import Network.HTTP.Types.URI
-import Web.Authenticate.OAuth (signOAuth, newCredential, emptyCredential, newOAuth, OAuth(..))
+import Web.Authenticate.OAuth (signOAuth, newCredential, emptyCredential, injectVerifier, newOAuth, OAuth(..))
 
 import QuickBooks.Logging (logAPICall, Logger)
 
@@ -38,9 +39,9 @@ getAccessTokensRequest :: ( ?apiConfig :: APIConfig
                                                                                   --   for a resource-owner
 getAccessTokensRequest verifier tempToken =
   let  ?apiConfig = ?apiConfig { oauthToken = token tempToken, oauthSecret = tokenSecret tempToken }
-       in return.handleQuickBooksTokenResponse "Couldn't get temporary tokens" =<< tokensRequest
+       in return.handleQuickBooksTokenResponse "Couldn't get access tokens" =<< tokensRequest
   where
-    tokensRequest = getTokens accessTokenURL "?oauth_verifier=" (unpack $ unOAuthVerifier verifier) oauthSignRequest
+    tokensRequest = getTokens accessTokenURL "?oauth_token=" (toString $ token tempToken) (oauthSignRequestWithVerifier verifier tempToken)
 
 
 getTokens :: ( ?apiConfig :: APIConfig
@@ -54,9 +55,20 @@ getTokens :: ( ?apiConfig :: APIConfig
 getTokens tokenURL parameterName parameterValue signRequest = do
   request  <- parseUrl $ concat [tokenURL, parameterName, parameterValue]
   request' <- signRequest request { method="POST", requestBody = RequestBodyLBS "" }
+  print request'
   response <- httpLbs request' ?manager
   logAPICall request'
   return $ tokensFromResponse (responseBody response)
+
+
+oauthSignRequestWithVerifier :: (?apiConfig :: APIConfig) => OAuthVerifier -> OAuthToken -> Request -> IO Request
+oauthSignRequestWithVerifier verifier tempTokens = signOAuth oauthApp credsWithVerifier
+  where
+    credentials       = newCredential (token tempTokens)
+                                      (tokenSecret tempTokens)
+    credsWithVerifier = injectVerifier (encodeUtf8 $ unOAuthVerifier verifier) credentials
+    oauthApp          = newOAuth { oauthConsumerKey    = consumerToken ?apiConfig
+                                 , oauthConsumerSecret = consumerSecret ?apiConfig }
 
 
 oauthSignRequest :: (?apiConfig :: APIConfig) => Request -> IO Request
@@ -82,7 +94,7 @@ temporaryTokenURL = "https://oauth.intuit.com/oauth/v1/get_request_token"
 
 handleQuickBooksTokenResponse :: String -> Maybe OAuthToken -> Either String (QuickBooksResponse OAuthToken)
 handleQuickBooksTokenResponse _ (Just tokensInResponse) = Right $ QuickBooksAuthResponse tokensInResponse
-handleQuickBooksTokenResponse errorMessage Nothing      = Left  $ errorMessage
+handleQuickBooksTokenResponse errorMessage Nothing      = Left errorMessage
 
 tokensFromResponse :: BSL.ByteString -> Maybe OAuthToken
 tokensFromResponse response = OAuthToken <$> lookup "oauth_token" responseParams
